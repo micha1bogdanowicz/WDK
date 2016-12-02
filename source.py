dae5c296cdaf5d4de6235b331ee743a7859898e5
bogdi <anondhc@gmail.com>
2016-12-03 00:01:30 +0100

    #Znaleziony Crib vs pozostale cipher text
    Crib: Pułkownik
    Z uwagi na
    Pułkownik
    Mieszkał
    Jego asyst
    To jego ob
    Czeski fun
    Kovacs odp
    Ta z pozor
    Czesi wied
    W pogotowi

--- source.py
+++ source.py
@@ -9,14 +9,18 @@ def xor_strings(bin_a, bin_b):
     return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(bin_a, bin_b))
 
 def main():
-    x = list[1].decode('hex')
-    b = list[2].decode('hex')
-    a=xor_strings(x,b)
-    print a.encode('hex')
+    a=[]
+    for element in list:
+        x = list[0].decode('hex')
+        b = element.decode('hex')
+        a.append(xor_strings(x,b))
+    a.pop(0)
+
     crib=raw_input("Crib: ").decode('CP852')
     crib=crib.encode('utf-8')
-    s= xor_strings(a,crib)
-    print s.decode('utf-8')
+    for element in a:
+        s= xor_strings(element,crib)
+        print s.decode('utf-8')
 
 def load_from_file():
     try:
