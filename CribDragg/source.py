efe7a993f9b55302394928d6db5e320c20c6bd0d
bogdi <anondhc@gmail.com>
2016-12-07 02:49:26 +0100

    optymalizacja

--- CribDragg/source.py
+++ CribDragg/source.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 
-import linecache
-import binascii
 list = []
 
 def xor_strings(bin_a, bin_b):
@@ -16,25 +14,14 @@ def main():
         x = list[crib_ciph].decode('hex')
         b = element.decode('hex')
         a.append(xor_strings(x,b))
-    #a.pop(0)
 
     crib=raw_input("Crib: ").decode('CP852')
     crib=crib.encode('utf-8')
     i=0
     for element in a:
-        if(len(crib)<len(element)):
             s= xor_strings(element,crib)
-            print str(i).encode("utf-8")+' '+ s.decode('utf-8','ignore')
-        else:
-            try:
-                s = xor_strings(element, crib)
-                print str(i) +' '+ s.decode('utf-8','ignore') + ' ####'
-
-            finally:
-                "####KONIEC CIPHER_TXT####"
-        i = i + 1
-
-
+            print str(i)+' '+ s.decode('utf-8',errors='ignore').encode("utf-8")
+            i = i + 1
 
 def load_from_file():
     try:
