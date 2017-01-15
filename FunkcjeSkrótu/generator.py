5d291791c157e6b6c925b8b1833511368708f69b
micha1bogdanowicz <polrider@protonmail.com>
2017-01-15 19:52:54 +0100

    md5 i sha256 z uzyciem Crypto.Hash

---
+++ FunkcjeSkrótu/generator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 2.7 py
+import sys
+from Crypto.Hash import MD5, SHA256
+
+
+for arg in sys.argv[1:]:
+    Crypt5 = MD5.new()
+    Crypt256 = SHA256.new()
+
+    file = open(arg,'r') #na windowsie dla binarek 'rb'
+    text = file.read()
+    Crypt5.update(text)
+    Crypt256.update(text)
+    print "Plik: ",arg
+    print "Twoja funkcja skrotu(md5): ",Crypt5.hexdigest()
+    print "Twoja funkcja skrotu(sha256): ",Crypt256.hexdigest()
+
+    file.close()
+
+
